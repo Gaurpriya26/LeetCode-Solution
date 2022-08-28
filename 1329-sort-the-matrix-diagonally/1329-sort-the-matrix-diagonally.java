0c01c14f39034a3dafd19028a5384d6e58197594
Gaurpriya26 <101595226+Gaurpriya26@users.noreply.github.com>
2022-08-28 18:42:44 +0530

    Time: 11 ms (54.17%), Space: 47.1 MB (84.11%) - LeetHub

---
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int[][] diagonalSort(int[][] mat) {
+        for(int col=0;col<mat[0].length;col++)
+        for(int row=0;row<mat.length;row++){
+            int i=0;
+            for(int j=col;j<mat[0].length-1 && i<mat.length-1;j++,i++){
+                if(mat[i][j]>mat[i+1][j+1]){
+                    int temp=mat[i][j];
+                    mat[i][j]=mat[i+1][j+1];
+                    mat[i+1][j+1]=temp;
+                }
+            }
+        }
+           for(int row = 1 ; row < mat.length; row ++){
+        for (int i = row ; i < mat.length - 1 ; i++){
+            int col = 0;
+            for (int j = row ; j < mat.length - 1 && col < mat[0].length - 1 ; j++ , col ++)
+            {
+                if (mat[j][col] > mat[j + 1][col + 1])
+                {
+                    int temp = mat[j][col];
+                    mat[j][col] = mat[j+1][col+1];
+                    mat[j+1][col+1] = temp;
+                }
+            }
+        }
+    }
+        return mat;
+    }
+}
+class Main{
+    public static void main(String args[]){
+        int m=100,n=100;
+        int[][] mat=new int[m][n];
+        Solution s=new Solution();
+        System.out.print(s.diagonalSort(mat));
+    }
+}
+//     for(int col = 0 ; col < n -1  ; col++)
+//     {
+        
+//         for(int i = 0 ; i < m ; i++)
+//         {
+//             int row = 0;
+            
+//             for(int j = col ; j < n - 1 && row < m - 1 ; j++ , row++)
+//             {
