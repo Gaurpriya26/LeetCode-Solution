3ea67f0dc33365a4ced0a56706a48482be988b5c
Gaurpriya26 <101595226+Gaurpriya26@users.noreply.github.com>
2022-08-30 16:00:38 +0530

    Time: 2 ms (89.06%), Space: 45.9 MB (50.14%) - LeetHub

---
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+              int m = grid.length;
+        int n = grid[0].length;
+        
+        int[][] dp = new int[m][n];
+        
+        return minPathSumHelper(grid, m-1, n-1, dp);
+    }
+    
+    public int minPathSumHelper(int[][] grid, int i, int j, int[][] dp) {
+        if(i==0 && j==0){
+            return grid[0][0];
+        } 
+        
+        if(i<0 || j<0){
+            return 20002;
+        }
+        
+        if(dp[i][j]!=0) return dp[i][j];
+
+        
+        int left = grid[i][j] + minPathSumHelper(grid, i, j-1, dp);
+        int up = grid[i][j] + minPathSumHelper(grid, i-1, j, dp);
+        
+        return dp[i][j] = Math.min(left,up);
+    }
+}
+class Main{
+    public static void main(String args[]){
+        int m=200,n=200;
+        int[][] grid=new int[m][n];
+        Solution s=new Solution();
+        System.out.print(s.minPathSum(grid));
+    }
+}
