4b207dd56a35710d5e8b6375327f30219657d707
Gaurpriya26 <101595226+Gaurpriya26@users.noreply.github.com>
2022-10-04 16:17:08 +0530

    Time: 7 ms (28.03%), Space: 52.9 MB (77.76%) - LeetHub

--- 143-reorder-list/143-reorder-list.java
+++ 143-reorder-list/143-reorder-list.java
@@ -39,26 +39,3 @@ class Solution {
         }
     }
 }
-
-//         ListNode preMid = slow;
-//         ListNode cur = slow.next;//cur is mid
-//         while(cur.next != null){
-//             ListNode next = cur.next;
-//             cur.next = next.next;
-//             next.next = preMid.next;
-//             preMid.next = next;
-//         }
-        
-//         ListNode p1 = head;
-//         ListNode p2 = preMid.next;
-        
-//         while(p1 != preMid){
-//             preMid.next = p2.next;
-//             p2.next = p1.next;
-//             p1.next = p2;
-//             p1 = p2.next;
-//             p2 = preMid.next;
-//         }
-//     }
-        
-        
