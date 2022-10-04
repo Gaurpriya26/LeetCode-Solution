f10dd5246a15d7d4849f03f6afb8723322221ffe
Gaurpriya26 <101595226+Gaurpriya26@users.noreply.github.com>
2022-10-04 16:13:09 +0530

    Time: 3 ms (53.65%), Space: 52.8 MB (79.54%) - LeetHub

---
+++ 143-reorder-list/143-reorder-list.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if(head==null || head.next==null) return;
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode premid=slow;
+        ListNode cur=premid.next;
+        
+        while(cur.next!=null){
+            ListNode next=cur.next;
+            cur.next=next.next;
+            next.next= premid.next;
+            premid.next=next;
+        }
+        
+        ListNode p1=head;
+        ListNode p2=premid.next;
+        
+        while(p1!=premid){
+            premid.next=p2.next;
+            p2.next=p1.next;
+            p1.next=p2;
+            p1=p2.next;
+            p2=premid.next;
+        }
+    }
+}
+
+//         ListNode preMid = slow;
+//         ListNode cur = slow.next;//cur is mid
+//         while(cur.next != null){
+//             ListNode next = cur.next;
+//             cur.next = next.next;
+//             next.next = preMid.next;
+//             preMid.next = next;
+//         }
+        
+//         ListNode p1 = head;
+//         ListNode p2 = preMid.next;
+        
+//         while(p1 != preMid){
+//             preMid.next = p2.next;
+//             p2.next = p1.next;
+//             p1.next = p2;
+//             p1 = p2.next;
+//             p2 = preMid.next;
+//         }
+//     }
+        
+        
