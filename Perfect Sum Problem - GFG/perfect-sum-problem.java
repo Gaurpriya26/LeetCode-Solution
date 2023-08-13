e34e1536bb8eec6b9b91e869afcb663db6645d8a
Gaurpriya26 <101595226+Gaurpriya26@users.noreply.github.com>
2023-08-13 19:52:20 +0530

    Added solution - LeetHub

---
+++ Perfect Sum Problem - GFG/perfect-sum-problem.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n = sc.nextInt();
+                    int sum = sc.nextInt();
+                    int arr[] = new int[n];
+                    for(int i = 0;i<n;i++)
+                    arr[i] = sc.nextInt();
+                    
+                    Solution ob = new Solution();
+                    System.out.println(ob.perfectSum(arr,n,sum));
+                }
+        }
+}    
+// } Driver Code Ends
+
+
+class Solution{
+      static final int mod=1000000000+7;
+	public int perfectSum(int arr[],int n, int sum) 
+	{ 
+	    int[][] dp=new int[n+1][sum+1];
+	   
+	    for(int i=0;i<n+1;i++){
+            dp[i][0]=1;
+	    }
+	    for(int i=1;i<n+1;i++){
+	        for(int j=0;j<sum+1;j++){
+	            if(j>=arr[i-1]){
+	                dp[i][j]=(dp[i-1][j-arr[i-1]]%mod + dp[i-1][j]%mod)%mod;
+	            }
+	            else
+	            dp[i][j]=dp[i-1][j]%mod;
+	        }
+	    }
+	    return dp[n][sum]%mod;
+	} 
+}
